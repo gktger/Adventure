43a9b054fadef4cd50101f24bbc761b0987ad999
Goktug Ercaliskan <goktug.ercaliskan@BK-LAB204-IS025>
2019-04-08 12:12:35 +0300

    "SQL BAGLANTISI SAGLANDI"

---
+++ Adventure.DAL/DataAccessLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure.DAL
+{
+   public class DataAccessLayer
+    {
+        static SqlConnection connection;
+
+
+        static DataAccessLayer()
+        {
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"].ToString();
+            connection = new SqlConnection(connectionString);
+        }
+
+    }
+}
